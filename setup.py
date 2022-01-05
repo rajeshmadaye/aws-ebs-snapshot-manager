ab43eebe0b0c12a3a681f1e069b15e8d47d62d8b
rajeshmadaye <rajeshmadaye@yahoo.com>
2022-01-05 12:00:51 -0500

    First draft version uploaded

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='snapshotManager',
+    version='0.1',
+    scripts=['snapshotManager.py'] ,
+    author="Rajesh Madaye",
+    author_email="rajeshmadaye@yahoo.com",
+    description="AWS Snapshot management package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rajeshmadaye/aws-snapshot-mgr.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+    ],
+ )
